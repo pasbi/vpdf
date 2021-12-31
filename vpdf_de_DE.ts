05b3d802aa0a2aafad69d736dc76a23b85c750a4
Pascal Bies <pascal.bies@k-lens.de>
2021-12-31 01:14:56 +0100

    update german translation

--- vpdf_de_DE.ts
+++ vpdf_de_DE.ts
@@ -6,59 +6,59 @@
     <message>
         <location filename="calibrationdialog.ui" line="20"/>
         <source>Calibrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalibrieren</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="41"/>
         <location filename="calibrationdialog.ui" line="122"/>
         <location filename="calibrationdialog.ui" line="136"/>
         <source>calibrate</source>
-        <translation type="unfinished"></translation>
+        <translation>Kalibrieren</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="55"/>
         <source>mm</source>
-        <translation type="unfinished"></translation>
+        <translation>mm</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="60"/>
         <source>cm</source>
-        <translation type="unfinished"></translation>
+        <translation>cm</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="65"/>
         <source>m</source>
-        <translation type="unfinished"></translation>
+        <translation>m</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="70"/>
         <source>km</source>
-        <translation type="unfinished"></translation>
+        <translation>km</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="78"/>
         <source>&amp;Unit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Einheit</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="95"/>
         <source>Δ:</source>
-        <translation type="unfinished"></translation>
+        <translation>Δ:</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="115"/>
         <source>Δx:</source>
-        <translation type="unfinished"></translation>
+        <translation>Δx:</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="129"/>
         <source>Δy:</source>
-        <translation type="unfinished"></translation>
+        <translation>Δy:</translation>
     </message>
     <message>
         <location filename="calibrationdialog.ui" line="156"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>Abbrechen</translation>
     </message>
 </context>
 <context>
@@ -66,84 +66,79 @@
     <message>
         <location filename="mainwindow.ui" line="14"/>
         <source>vpdf</source>
-        <translation type="unfinished"></translation>
+        <translation>vpdf</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="48"/>
         <source>&amp;Page:</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Seite:</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="61"/>
         <source>Page </source>
-        <translation type="unfinished"></translation>
+        <translation>Seite </translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="68"/>
         <source>&amp;Zoom</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Zoom</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="102"/>
-        <source>&amp;Calibration:</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="112"/>
-        <source>c&amp;alibrate ...</source>
-        <translation type="unfinished"></translation>
+        <location filename="mainwindow.ui" line="159"/>
+        <source>&amp;Open ...</source>
+        <translation>&amp;Öffnen ...</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="80"/>
         <location filename="mainwindow.ui" line="83"/>
         <source>-</source>
-        <translation type="unfinished"></translation>
+        <translation>-</translation>
     </message>
     <message>
         <location filename="mainwindow.ui" line="90"/>
         <location filename="mainwindow.ui" line="93"/>
         <source>+</source>
-        <translation type="unfinished"></translation>
+        <translation>+</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="156"/>
+        <location filename="mainwindow.ui" line="105"/>
+        <source>&amp;calibrate ...</source>
+        <translation>&amp;Kalibrieren ...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
         <source>&amp;File</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="166"/>
-        <source>&amp;Open</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="mainwindow.ui" line="169"/>
+        <location filename="mainwindow.ui" line="162"/>
         <source>Ctrl+O</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+O</translation>
     </message>
     <message>
-        <location filename="mainwindow.ui" line="174"/>
+        <location filename="mainwindow.ui" line="167"/>
         <source>&amp;Exit</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Exit</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="37"/>
         <source>Failed to open PDF document %1</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffnen der PDF fehlgeschlagen: %1</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="59"/>
         <source>Open PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF öffnen</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="61"/>
         <source>PDF documents (*.pdf)</source>
-        <translation type="unfinished"></translation>
+        <translation>PDF Dokumente (*.pdf)</translation>
     </message>
     <message>
         <location filename="mainwindow.cpp" line="37"/>
         <source>Failed to open PDF</source>
-        <translation type="unfinished"></translation>
+        <translation>Öffnen der PDF fehlgeschlagen</translation>
     </message>
 </context>
 </TS>
